1ce8739c7dc16298af84bef807b1f26ef10700fb
mbp1980 <73567442+mbp1980@users.noreply.github.com>
2021-03-12 18:24:12 -0700

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,42 +1,43 @@
 const passport = require("passport"),
-LocalStrategy = require("passport-local").Strategy,
-Models = require("./models.js"),
-passportJWT = require("passport-jwt");
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
 
-let users = Models.User,
-JWTStrategy = passportJWT.Strategy,
-ExtractJWT = passportJWT.ExtractJwt;
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
 
-//LocalStrategy is checking if Username is valid within Mongoose
-passport.use(new LocalStrategy({
-    usernameField: "Username",
-    passwordField: "Password",
+
+  passport.use(new LocalStrategy({
+  usernameField: "Username",
+  passwordField: "Password"
 }, (username, password, callback) => {
-    console.log(username + " " + password);
-    users.findOne({ Username: username }, (err, user) => {
-        if (error) {
-            console.log(error);
-        }
+  console.log(username + "  " + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
 
-        if (!user) {
-            console.log("incorrect username");
-            return callback(null, false, {message: "incorrect username or password"});
-        }
+    if (!user) {
+      console.log("incorrect username");
+      return callback(null, false, {message: "Incorrect username or password."});
+    }
 
-        console.log("finished");
-        return callback(null, user);
-    });
+    console.log("finished");
+    return callback(null, user);
+  });
 }));
 
 passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: "your_jwt_secret"
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "your_jwt_secret"
 }, (jwtPayload, callback) => {
-    return users.findById(jwtPayload._id)
-    .then ((user) => {
-        return callback(null, user);
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
     })
     .catch((error) => {
-        return callback(error)
+      return callback(error)
     });
 }));
